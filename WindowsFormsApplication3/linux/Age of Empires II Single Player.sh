d4f691750e11b686bad38f3e15aef9014602bdf0
gregstein <shock-feeling@hotmail.fr>
2021-03-15 12:34:12 +0100

    Unix format shell scripts

---
+++ WindowsFormsApplication3/linux/Age of Empires II Single Player.sh
@@ -0,0 +1,1 @@
+WINEPREFIX="$HOME/win32" WINEARCH=win32 wine "/home/$USER/.wine/drive_c/users/$USER/Application Data/Microsoft Games/Age of Empires ii/Age2_x1/age2_x1_fixed.exe"
--- WindowsFormsApplication3/linux/Auto-Installer-Ubuntu-19.10.sh
+++ WindowsFormsApplication3/linux/Auto-Installer-Ubuntu-19.10.sh
@@ -55,9 +55,8 @@ echo Installing Scroll Fix For AoE2 And Creating a Shortcut In Your Desktop
 echo ===================================================
 
 wget https://github.com/SFTtech/sftscrollbugfixer/releases/download/v1.4.0.0/age2_x1_fixed.tar.gz -o aoe2fix.tar.gz
-tar -xvzf aoe2fix.tar.gz -C /home/$USER/.wine/drive_c/users/$USER/Application Data/Microsoft Games/Age of Empires ii/Age2_x1/
-wget https://raw.githubusercontent.com/gregstein/AoE2Tools/master/WindowsFormsApplication3/aoe2-linux-shortcut.lnk -o /home/$USER/Desktop/Age of Empires II Single Player.lnk
-
+tar -xf aoe2fix.tar.gz --strip-components 1 --directory /home/$USER/.wine/drive_c/users/$USER/Application Data/Microsoft Games/Age of Empires ii/Age2_x1/
+wget "https://raw.githubusercontent.com/gregstein/AoE2Tools/master/WindowsFormsApplication3/linux/Age of Empires II Single Player.sh" -o "/home/$USER/Desktop/Age of Empires II Single Player.sh" && chmod +x "Age of Empires II Single Player.sh"
 
 echo ===================================================
 printf "${gr}:: Installation Successful! :: \n"
