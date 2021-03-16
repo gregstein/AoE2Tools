ca5ff31e21882063b2e320f67d4b62afdea278aa
gregstein <shock-feeling@hotmail.fr>
2021-03-16 12:35:56 +0100

    Add files via upload

--- WindowsFormsApplication3/linux/AoE2Tools.sh
+++ WindowsFormsApplication3/linux/AoE2Tools.sh
@@ -1,1 +1,2 @@
-WINEPREFIX="$HOME/win32" WINEARCH=win32 wine "/home/$USER/win32/dosdevices/c:/users/$USER/Application Data/AoE2Tools/AoE2Tools.exe"
+#!/usr/bin/env bash
+WINEPREFIX="$HOME/win32" WINEARCH=win32 wine "/home/$USER/win32/dosdevices/c:/users/$USER/Application Data/AoE2Tools/Launcher.exe"
--- WindowsFormsApplication3/linux/Steam.sh
+++ WindowsFormsApplication3/linux/Steam.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 WINEPREFIX="$HOME/win32" WINEARCH=win32 wine "/home/$USER/win32/dosdevices/c:/Program Files/Steam/steam.exe"
